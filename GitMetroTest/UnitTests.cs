23fbfda7d7aa310834433d0710b8e17a7c1b7281
Jacob Logas <Jakeman893@ufl.edu>
2017-01-09 21:39:19 -0500

    Restarting

---
+++ GitMetroTest/UnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace GitMetroTest
+{
+    [TestClass]
+    public class GitHubServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
