15b736716e4d43acda9ee5487181308a06b829b4
Jacob Logas <Jakeman893@ufl.edu>
2017-01-09 21:18:56 -0500

    Add project files.

---
+++ GitMetro/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using GitMetro.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace GitMetro.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
